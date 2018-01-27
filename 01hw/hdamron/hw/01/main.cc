1c4e6d7a2c4e1fb85dd0881f825756eb70ec5d3d
Hunter Damron <hdamron1594@yahoo.com>
2018-01-27 14:02:24 -0500

    Completed 01hw (directory structure may be slightly wrong)

---
+++ 01hw/hdamron/hw/01/main.cc
@@ -0,0 +1,36 @@
+/**
+ * Created by Hunter Damron
+ * Copyright 2018
+ */
+
+/**
+ * Application accepts one positive or negative integer from stdin without
+ * prompt and outputs the sum of its digits with the same sign as the input.
+ */
+
+#include <iostream>
+using std::cin;
+using std::cout;
+using std::endl;
+
+int main(int argc, char** argv) {
+    int num;
+    cin >> num;
+
+    bool negative = num < 0;
+    if (negative)
+        num = -num;
+
+    int sum = 0;
+    while (num > 0) {
+        sum += num % 10;
+        num /= 10;
+    }
+
+    if (negative)
+        sum = -sum;
+
+    cout << sum << endl;
+
+    return 0;
+}
